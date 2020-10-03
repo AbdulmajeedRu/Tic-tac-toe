77f5b661477bdaec2ae9c32ac442f06e635d32e5
AbdulmajeedRu <44317320+AbdulmajeedRu@users.noreply.github.com>
2020-10-03 22:57:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,102 +1,146 @@
-player1 = [0,0,0,0,0,0,0,0,0]
-player2 = [0,0,0,0,0,0,0,0,0]
-board = [[0,0,0],
-         [0,0,0],
-         [0,0,0]]
+board = [[1,2,3],
+         [4,5,6],
+         [7,8,9]]
+checklist =[]
 
 turn = 1
+counter = 0
+
+def restart():
+    global board
+    global checklist
+    global turn
+    global counter
+    board = [[1, 2, 3],
+             [4, 5, 6],
+             [7, 8, 9]]
+
+    checklist = []
+
+    turn = 1
+    counter = 0
+
 
 def boardp():
-    ibrahim = 1
-    for i in range(3):
-        for j in range(3):
-            board[i][j] = ibrahim
-            ibrahim = ibrahim + 1
+
     print("  ", board[0][0], "  |  ", board[0][1], "  |  ", board[0][2], " ")
     print("  ____________________")
     print("  ", board[1][0], "  |  ", board[1][1], "  |  ", board[1][2], " ")
     print("  ____________________")
     print("  ", board[2][0], "  |  ", board[2][1], "  |  ", board[2][2], " ")
+    print("")
 
-def play(turn=turn):
-
+def play():
+    global turn
+    global counter
+    global checklist
     boardp()
 
+
     if turn == 1:
-        p = int(input("Player 1 turn, choose an area to play:"))
+        try:
+            p = int(input("Player 1 turn, choose an area to play:"))
+            print("")
+        except:
+            print("That's not a valid option, please input an integer.")
+            pass
+            play()
+
+
+        if p in checklist:
+            print("Area is already taken, choose a different one.")
+            play()
         for i in range(3):
             for j in range(3):
+
                 if p == board[i][j]:
-                    board[i][j] = 1
-        player1[p-1] = 1
+                    board[i][j] = 'X'
+                    checklist.append(p)
+
+
         turn = 2
+        counter = counter + 1
         check(p)
+
     else:
-        p = int(input("Player 2 turn, choose an area to play:"))
+        try:
+            p = int(input("Player 2 turn, choose an area to play:"))
+            print("")
+        except:
+            print("That's not a valid option, please input an integer.")
+            pass
+            play()
+        if p in checklist:
+            print("Area is already taken, choose a different one.")
+            play()
         for i in range(3):
             for j in range(3):
                 if p == board[i][j]:
-                    board[i][j] = 2
-        player2[p-1] = 1
+                    board[i][j] = 'O'
+                    checklist.append(p)
+
+
         turn = 1
+        counter = counter + 1
         check(p)
 
 
 def check(p):
-    if board[0][0] == 1 & board[0][1] == 1 & board[0][2] == 1:
-        print("Player 1 wins.")
-        exit(0)
-    elif board[1][0] == 1 & board[1][1] == 1 & board[1][2] == 1:
-        print("Player 1 wins.")
-        exit(0)
-    elif board[2][0] == 1 & board[2][1] == 1 & board[2][2] == 1:
-        print("Player 1 wins.")
-        exit(0)
-    elif (board[0][0] == 1 & board[1][0] == 1 & board[2][0] == 1):
-        print("Player 1 wins.222")
-        exit(0)
-    elif board[0][1] == 1 & board[1][1] == 1 & board[2][1] == 1:
-        print("Player 1 wins.")
-        exit(0)
-    elif board[0][2] == 1 & board[1][2] == 1 & board[1][3] == 1:
-        print("Player 1 wins.")
-        exit(0)
-    elif (board[0][0] == 1) & (board[1][1] == 1) & (board[2][2] == 1):
-        print("Player 1 wins.111")
-        exit(0)
-    elif board[0][2] == 1 & board[1][1] == 1 & board[2][0] == 1:
-        print("Player 1 wins.")
-        exit(0)
-    elif board[0][0] == 2 & board[0][1] == 2 & board[0][2] == 2:
-            print("Player 2 wins.")
-            exit(0)
-    elif board[1][0] == 2 & board[1][1] == 2 & board[1][2] == 2:
-            print("Player 2 wins.")
-            exit(0)
-    elif board[2][0] == 2 & board[2][1] == 2 & board[2][2] == 2:
-            print("Player 2 wins.")
-            exit(0)
-    elif board[0][0] == 2 & board[1][0] == 2 & board[2][0] == 2:
-            print("Player 2 wins.")
-            exit(0)
-    elif board[0][1] == 2 & board[1][1] == 2 & board[2][1] == 2:
-            print("Player 2 wins.")
-            exit(0)
-    elif (board[0][2] == 2 & board[1][2] == 2 & board[1][3] == 2):
-            print("Player 2 wins.")
-            exit(0)
-    elif board[0][0] == 2 & board[1][1] == 2 & board[2][2] == 2:
-            print("Player 2 wins.")
-            exit(0)
-    elif (board[0][2] == 2 & board[1][1] == 2 & board[2][0] == 2):
-            print("Player 2 wins.")
-            exit(0)
-    else:
-        play()
+    global counter
+    for i in range(8):
+        for j in range(3):
+            if (board[j][0] == 'X') & (board[j][1] == 'X') & (board[j][2] == 'X'):
+                boardp()
+                print("Player 1 wins.")
+                exit(0)
+            elif (board[0][j] == 'X') & (board[1][j] == 'X') & (board[2][j] == 'X'):
+                boardp()
+                print("Player 1 wins.")
+                exit(0)
+            elif (board[0][0] == 'X') & (board[1][1] == 'X') & (board[2][2] == 'X'):
+                boardp()
+                print("Player 1 wins.")
+                exit(0)
+            elif (board[0][2] == 'X') & (board[1][1] == 'X') & (board[2][0] == 'X'):
+                boardp()
+                print("Player 1 wins.")
+                exit(0)
+            elif (board[j][0] == 'O') & (board[j][1] == 'O') & (board[j][2] == 'O'):
+                boardp()
+                print("Player 2 wins.")
+                exit(0)
+            elif (board[0][j] == 'O') & (board[1][j] == 'O') & (board[2][j] == 'O'):
+                boardp()
+                print("Player 2 wins.")
+                exit(0)
+            elif (board[0][0] == 'O') & (board[1][1] == 'O') & (board[2][2] == 'O'):
+                boardp()
+                print("Player 2 wins.")
+                exit(0)
+            elif (board[0][2] == 'O') & (board[1][1] == 'O') & (board[2][0] == 'O'):
+                boardp()
+                print("Player 2 wins.")
+                exit(0)
+            else:
+                if counter >= 9:
+                    boardp()
+                    print("Result is draw, finishing game.")
+                    exit(0)
+                continue
 
 
-print("This is an XO game made using python.\nFirst player will be X, Second player will be O"
+
+
+print("This is a Tic Tac Toe game made using python.\nFirst player will be X, Second player will be O"
       " \nGame board will be numbered from 1 to 9, each player will take a turn to choose their selected square.")
 
-while(1):
+for i in range(8):
     play()
+
+x = input("\n Enter 1 to play again, anything else to stop playing.")
+if (x == '1'):
+    restart()
+    for i in range(8):
+        play()
+else:
+    exit(0)
